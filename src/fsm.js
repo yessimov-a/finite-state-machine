71f26f7dcd536d3633b4978b7ef7e6e08082e0fe
yessimov-a <a.yessimov98@gmail.com>
2019-10-11 12:03:18 +0600

    feat: complete fsm project

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,50 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) throw new Error();
+        this._initial = config.initial;
+        this._state = config.initial;
+        this._states = config.states;
+        this._nextState = null;
+        this._prevState = null;
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this._state
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (!this._states[state]) throw new Error();
+        this._prevState = this._state;
+        this._state = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (!this._states[this._state].transitions[event]) throw new Error();
+        this._prevState = this._state;
+        this._state = this._states[this._state].transitions[event];
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this._state = this._initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,28 +54,53 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (!event) {
+            return Object.keys(this._states);
+        } else {
+            let result = [];
+            for (let key in this._states) {
+                if (event in this._states[key].transitions) {
+                    result = [...result, key]
+                }
+            }
+            return result;
+        }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this._prevState === null) return false;
+        this._nextState = this._state;
+        this._state = this._prevState;
+        this._prevState = null;
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(this._nextState === null) return false;
+        this._state = this._nextState;
+        this._nextState = null;
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this._nextState = null;
+        this._prevState = null;
+
+    }
 }
 
 module.exports = FSM;
-
-/** @Created by Uladzimir Halushka **/
